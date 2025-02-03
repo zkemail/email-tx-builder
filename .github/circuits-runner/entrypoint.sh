44961ff319306b254597c75ecba32122c87cd089
wshino <wshinohara@gmail.com>
2025-02-03 23:23:07 +0900

    chore: update entrypoint.sh

--- .github/circuits-runner/entrypoint.sh
+++ .github/circuits-runner/entrypoint.sh
@@ -3,7 +3,13 @@
 REPO_URL=${REPO_URL:-"https://github.com/your/repo.git"}
 COMMIT_HASH=${COMMIT_HASH:-"your_commit_hash"}
 
+echo "Cloning repository: $REPO_URL"
 git clone $REPO_URL .
+echo "Checking out commit: $COMMIT_HASH"
 git checkout $COMMIT_HASH
 
-yarn install --frozen-lockfile && cd packages/circuits && yarn test --maxWorkers=75% --no-cache
+echo "Installing dependencies"
+yarn install --frozen-lockfile
+
+echo "Running tests"
+cd packages/circuits && yarn test --maxWorkers=75% --no-cache
