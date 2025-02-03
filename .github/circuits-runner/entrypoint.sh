4584ce706f0eb7c187ee2ee52a41394f4638f408
wshino <wshinohara@gmail.com>
2025-02-03 23:23:07 +0900

    chore: update entrypoint.sh
    chore: add image pull policy
    
    chore: add image pull policy
    
    chore: add env configuration

--- .github/circuits-runner/entrypoint.sh
+++ .github/circuits-runner/entrypoint.sh
@@ -3,7 +3,16 @@
 REPO_URL=${REPO_URL:-"https://github.com/your/repo.git"}
 COMMIT_HASH=${COMMIT_HASH:-"your_commit_hash"}
 
-git clone $REPO_URL .
+echo "Cloning repository: $REPO_URL"
+git clone $REPO_URL repo
+cd repo
+
+echo "Checking out commit: $COMMIT_HASH"
 git checkout $COMMIT_HASH
 
-yarn install --frozen-lockfile && cd packages/circuits && yarn test --maxWorkers=75% --no-cache
+echo "Installing dependencies"
+yarn install --frozen-lockfile
+
+echo "Running tests"
+cd packages/circuits
+yarn test --maxWorkers=75% --no-cache
