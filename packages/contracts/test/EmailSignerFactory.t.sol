b892efe1ee281f3ae1466605273d76888171cd07
zkfriendly <zkfriendly@proton.me>
2025-03-11 13:25:28 +0100

    test: Add tests for zero account salt and zero template id validation

--- packages/contracts/test/EmailSignerFactory.t.sol
+++ packages/contracts/test/EmailSignerFactory.t.sol
@@ -5,6 +5,32 @@ import "forge-std/Test.sol";
 import "../src/EmailSignerFactory.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
+// Helper contract to test the InvalidTemplateId error
+contract MockEmailSignerFactory is EmailSignerFactory {
+    constructor(
+        address _implementation,
+        address _dkimRegistry,
+        address _verifier
+    ) EmailSignerFactory(_implementation, _dkimRegistry, _verifier) {}
+
+    // Function that will try to initialize with a zero templateId
+    function deployWithZeroTemplateId(
+        bytes32 accountSalt
+    ) external returns (address) {
+        address clone = Clones.cloneDeterministic(implementation, accountSalt);
+
+        // Initialize with zero templateId
+        EmailSigner(clone).initialize(
+            accountSalt,
+            dkimRegistry,
+            verifier,
+            0 // Zero templateId should cause a revert
+        );
+
+        return clone;
+    }
+}
+
 contract EmailSignerFactoryTest is Test {
     EmailSignerFactory factory;
     EmailSigner implementation;
@@ -51,6 +77,30 @@ contract EmailSignerFactoryTest is Test {
             address(dkim),
             address(0)
         );
+    }
+
+    function testDeployRevertInvalidAccountSalt() public {
+        // Test with zero account salt
+        bytes32 zeroSalt = bytes32(0);
+
+        vm.expectRevert(abi.encodeWithSignature("InvalidAccountSalt()"));
+        factory.deploy(zeroSalt);
+    }
+
+    function testDeployRevertInvalidTemplateId() public {
+        // Mock the EmailSigner implementation to make templateId calculation return 0
+        // We need to deploy a mock factory that will generate a zero templateId
+
+        // Create a mock implementation that we can use to test the zero templateId case
+        MockEmailSignerFactory mockFactory = new MockEmailSignerFactory(
+            address(implementation),
+            address(dkim),
+            address(verifier)
+        );
+
+        // The mock factory will try to initialize with a zero templateId
+        vm.expectRevert(abi.encodeWithSignature("InvalidTemplateId()"));
+        mockFactory.deployWithZeroTemplateId(TEST_ACCOUNT_SALT);
     }
 
     function testPredictAddress() public view {
